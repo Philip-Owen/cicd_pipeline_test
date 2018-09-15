0e110a5c3ed909016d82bbe59d336a1c30b3b421
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-09-15 16:15:04 -0500

    testing issue with docker deployment

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
@@ -8,3 +8,4 @@ fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
 echo $TRAVIS_REPO_SLUG
+echo $DOCKER_USER
